15b5e861546abf99431b1207619a38caa07c6a0e
kumarnvm <kumarnvm@yahoo.com>
2016-07-05 06:37:52 -0500

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+     
+.controller('cameraTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+      
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('pageCtrl', function($scope) {
+
+})
+ 
